bf3ea514aa5d12814e895e90304837a891aae4cf
Lensku <henri.lencioni@aalto.fi>
2020-02-19 13:01:57 +0200

    Gitignore, doge file separated, and data function

---
+++ doge.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 10:54:35 2020
+
+@author: Henri Lencioni
+"""
+
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+def get_image_url(allowed_extension):
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+def pic(bot, update):
+    print("picture")
+    allowed_extension = ['jpg','jpeg','png']
+    url = get_image_url(allowed_extension)
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id = chat_id, photo = url)
+
+def vid(bot, update):
+    print("video")
+    allowed_extension = ['gif','mp4']
+    url = get_image_url(allowed_extension)
+    chat_id = update.message.chat_id
+    bot.sendAnimation(chat_id = chat_id, animation = url)
--- main.py
+++ main.py
@@ -6,62 +6,57 @@ Created on Tue Feb 18 14:55:57 2020
 """
 
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-import requests
-import re
+from doge import pic, vid
 import numpy as np
 from pandas_datareader import data
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()    
-    url = contents['url']
-    return url
-
-def get_image_url(allowed_extension):
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_url()
-        file_extension = re.search("([^.]*)$",url).group(1).lower()
-    return url
-
-def pic(bot, update):
-    print("picture")
-    allowed_extension = ['jpg','jpeg','png']
-    url = get_image_url(allowed_extension)
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id = chat_id, photo = url)
-
-def vid(bot, update):
-    print("video")
-    allowed_extension = ['gif','mp4']
-    url = get_image_url(allowed_extension)
-    chat_id = update.message.chat_id
-    bot.sendAnimation(chat_id = chat_id, animation = url)
-    
 def fetch(bot, update):
+    #pass arguments to fetch yahoo data???
+    global amazon, apple, index
     print('fetch')
-    tickers = ['AMZN', 'GOOG', '.INX']
-    start_date = '2000-01-01'
+    start_date = '2018-01-01'
     end_date = '2020-01-01'
     amazon = data.DataReader('AMZN', 'yahoo', start_date, end_date)
-    google = data.DataReader('AAPL', 'yahoo', start_date, end_date)
+    print('amazon')
+    apple = data.DataReader('AAPL', 'yahoo', start_date, end_date)
+    print('apple')
     index = data.DataReader('SPY', 'yahoo', start_date, end_date)
+    print('index')
     chat_id = update.message.chat_id
     bot.sendMessage(chat_id = chat_id, text = "Fetched the data!")
     
-def data(bot, update, args):
+def data_f(bot, update, args):
+    print('data')
+    chat_id = update.message.chat_id
+    if not args:
+        bot.sendMessage(chat_id = chat_id, text = "Please pass arguments")
     for arg in args:
-        print(arg)
-    
+        if arg not in ['amazon', 'apple', 'index']:
+            message = "Error: " + arg + " argument is not valid..." 
+            bot.sendMessage(chat_id = chat_id, text = message)
+        elif globals()[arg] is None:
+            message = "Error: Remember to fetch " + arg + "!"
+            bot.sendMessage(chat_id = chat_id, text = message)
+        else:
+            bot.sendMessage(chat_id = chat_id, text = arg)
+            bot.sendMessage(chat_id = chat_id, text = globals()[arg].tail().to_string())
+
+# def plot hae yksi ja plottaa
+# def plot hae argumenttien mukaan
+# plot joka näyttää myös moving average
+# plot joka standardoi muuttujat ja plottaa päällekkäin
 
 def main():
     updater = Updater('1084871037:AAFlb-Z9LXTsgot3gj4sOxaaE64M64dBUCM')
+    global amazon, apple, index
+    amazon, apple, index = None, None, None
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('pic',pic))
     dp.add_handler(CommandHandler('vid',vid))
     dp.add_handler(CommandHandler('fetch', fetch))
-    dp.add_handler(CommandHandler("data", data, pass_args = True))
+    dp.add_handler(CommandHandler("data", data_f, pass_args = True))
     updater.start_polling()
     updater.idle()
 
