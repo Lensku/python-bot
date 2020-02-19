a0a184df1ecbb5765c109a512b352ae91997a0b3
Lensku <henri.lencioni@aalto.fi>
2020-02-19 10:39:14 +0200

    Fetching data

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ Created on Tue Feb 18 14:55:57 2020
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
 import re
-import yfinance as yf
 import numpy as np
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()    
@@ -37,20 +39,29 @@ def vid(bot, update):
     chat_id = update.message.chat_id
     bot.sendAnimation(chat_id = chat_id, animation = url)
     
-def msft(bot, update):
-    msft = yf.Ticker("MSFT")
-    history = msft.history(period = "max")
-    print(msft)
-    print(history)
+def fetch(bot, update):
+    print('fetch')
+    tickers = ['AMZN', 'GOOG', '.INX']
+    start_date = '2000-01-01'
+    end_date = '2020-01-01'
+    amazon = data.DataReader('AMZN', 'yahoo', start_date, end_date)
+    google = data.DataReader('AAPL', 'yahoo', start_date, end_date)
+    index = data.DataReader('SPY', 'yahoo', start_date, end_date)
     chat_id = update.message.chat_id
-    bot.sendMessage(chat_id = chat_id, text = "test")
+    bot.sendMessage(chat_id = chat_id, text = "Fetched the data!")
+    
+def data(bot, update, args):
+    for arg in args:
+        print(arg)
+    
 
 def main():
     updater = Updater('1084871037:AAFlb-Z9LXTsgot3gj4sOxaaE64M64dBUCM')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('pic',pic))
     dp.add_handler(CommandHandler('vid',vid))
-    dp.add_handler(CommandHandler('msft',msft))
+    dp.add_handler(CommandHandler('fetch', fetch))
+    dp.add_handler(CommandHandler("data", data, pass_args = True))
     updater.start_polling()
     updater.idle()
 
