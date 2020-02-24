d7c9c4b2751fb8291cb53627fd80f62d567d440d
Lensku <henri.lencioni@aalto.fi>
2020-02-24 16:40:08 +0200

    Minor changes and fixes

--- data.py
+++ data.py
@@ -10,34 +10,31 @@ import pandas as pd
 from io import BytesIO
 from pandas_datareader import data
 
+def format(stock):
+    stock.insert(6, "close_ma_20", stock['Close'].rolling(20).mean())
+    stock.insert(7, "close_ma_100", stock['Close'].rolling(100).mean())
+    stock.insert(8, "standardized", stock['Close']/stock['Close'][0])
+
 def fetch(bot, update):
     #pass arguments to fetch yahoo data???
     global amazon, apple, index
     print('fetch')
     start_date = '2018-01-01'
     end_date = '2020-01-01'
-    # create amazon --> then function call for the extra measurements
     amazon = data.DataReader('AMZN', 'yahoo', start_date, end_date)
-    amazon.insert(6, "close_ma_20", amazon['Close'].rolling(20).mean())
-    amazon.insert(7, "close_ma_100", amazon['Close'].rolling(100).mean())
-    amazon.insert(8, "standardized", amazon['Close']/amazon['Close'][0])
-    print('amazon')
+    format(amazon)
     apple = data.DataReader('AAPL', 'yahoo', start_date, end_date)
-    apple.insert(6, "close_ma_20", apple['Close'].rolling(20).mean())
-    apple.insert(7, "close_ma_100", apple['Close'].rolling(100).mean())
-    apple.insert(8, "standardized", apple['Close']/apple['Close'][0])
-    print('apple')
+    format(apple)
     index = data.DataReader('SPY', 'yahoo', start_date, end_date)
-    index.insert(6, "close_ma_20", index['Close'].rolling(20).mean())
-    index.insert(7, "close_ma_100", index['Close'].rolling(100).mean())
-    index.insert(8, "standardized", index['Close']/index['Close'][0])
-    print('index')
+    format(index)
     chat_id = update.message.chat_id
     bot.sendMessage(chat_id = chat_id, text = "Fetched the data!")
 
 def data_f(bot, update, args):
     print('data')
     chat_id = update.message.chat_id
+    # funktio palauttaa true jos argumentti kunnossa, muuten false ja oikean viestin
+    # (continue hyppää seuraavaan)
     if not args:
         bot.sendMessage(chat_id = chat_id, text = "Please pass an argument")
     for arg in args:
@@ -64,6 +61,7 @@ def standardized(bot, update):
         ax.set_xlabel('Date')
         ax.set_ylabel('Closing price ($)')
         ax.legend()
+        # buffer komennot funktioon?
         buffer = BytesIO()
         fig.savefig(buffer, format = 'png')
         buffer.seek(0)
